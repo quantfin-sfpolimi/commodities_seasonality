d5fe420f00ed55180bf09eca8f6aa473d72fc08f
Alberto Toia <albetoiaa@gmail.com>
2024-05-12 16:04:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,13 @@ inputForm.addEventListener("submit", (e) => {
   let start = document.getElementById("startYear").value
   let end = document.getElementById("endYear").value
     
-    let input = {
-        "start": parseInt(start),
-        "end": parseInt(end),
-    }
+  let input = {
+    "start": parseInt(start),
+    "end": parseInt(end),
+  }
 
+  console.log(input)
 
-    console.log(input)
-
-
-    
-    // to be fixed. https:// should be removed, but when done it concatenates the acrual url with 127.0.0.1/800/...
-    url = 'https://127.0.0.1:8000/' + 'get-year/' + start + end
-    window.location.href = url
+  url = 'http://127.0.0.1:8000/' + 'get-year/' + start + end
+  window.location.href = url
 })
