98b8ea54acceffe4b997ca4850fb62fc4692abb0
giutala <giuliatala32@gmail.com>
2024-05-28 22:38:16 +0200

    wip

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.gzip import GZipMiddleware
 
-app=FastAPI()
-app.mount("/static",StaticFiles(directory="static"),name="static")
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.add_middleware(GZipMiddleware)
 
-templates=Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-async def index(request:Request):
-    return templates.TemplateResponse("base.html",{"request":request})
+async def index(request: Request):
+    return templates.TemplateResponse(name="base.html", request=request, context={"stock": "moneymaker"})
